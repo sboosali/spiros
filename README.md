b17c956cf7a221dd1319316b03d811afc197b3cf
Spiros Boosalis <samboosalis@gmail.com>
2016-05-20 14:04:37 +0300

    1st

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://secure.travis-ci.org/sboosali/spiros.svg)](http://travis-ci.org/sboosali/spiros)
+[![Hackage](https://img.shields.io/hackage/v/spiros.svg)](https://hackage.haskell.org/package/spiros)
+
+# spiros
+
+TODO
---
+++ sources/Prelude/Spiros.hs
@@ -0,0 +1,42 @@
+{-|
+
+
+see:
+
+<http://www.stephendiehl.com/posts/protolude.html>
+
+-}
+module Prelude.Spiros
+ ( module Prelude.Spiros
+
+ , module Control.DeepSeq
+ , module Data.Semigroup
+
+ , module GHC.Generics
+ , module Data.Data
+ , module Control.Arrow
+
+ , module Data.Function
+ ) where
+
+import Control.DeepSeq (NFData)
+import Data.Semigroup (Semigroup)
+
+import GHC.Generics (Generic)
+import Data.Data (Data)
+import Control.Arrow ((>>>))
+
+import Data.Function ((&))
+
+
+nothing :: (Monad m) => m ()
+nothing = return ()
+
+maybe2bool :: Maybe a -> Bool
+maybe2bool = maybe False (const True)
+
+either2maybe :: Either e a -> Maybe a
+either2maybe = either (const Nothing) Just
+
+either2bool :: Either e a -> Bool
+either2bool = either (const False) (const True)
