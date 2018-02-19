dbe489d428ec0c6f1700fde703cbdd90738b0f14
Spiros Boosalis <SamBoosalis@gmail.com>
2018-02-19 14:56:44 -0800

    bump to 0.0.1

--- default.nix
+++ default.nix
@@ -1,18 +1,17 @@
-{ mkDerivation, base, bytestring, clock, containers
-, data-default-class, deepseq, directory, hashable, mtl
-, prettyprinter, process, safe, safe-exceptions, split, stdenv, stm
-, string-conv, text, time, transformers, unordered-containers
-, vector, vinyl
+{ mkDerivation, base, bytestring, containers, data-default-class
+, deepseq, directory, hashable, mtl, prettyprinter, process, safe
+, safe-exceptions, split, stdenv, stm, string-conv, text, time
+, transformers, unordered-containers, vector, vinyl
 }:
 mkDerivation {
   pname = "spiros";
   version = "0.0.1";
   src = ./.;
   libraryHaskellDepends = [
-    base bytestring clock containers data-default-class deepseq
-    directory hashable mtl prettyprinter process safe safe-exceptions
-    split stm string-conv text time transformers unordered-containers
-    vector vinyl
+    base bytestring containers data-default-class deepseq directory
+    hashable mtl prettyprinter process safe safe-exceptions split stm
+    string-conv text time transformers unordered-containers vector
+    vinyl
   ];
   homepage = "http://github.com/sboosali/spiros#readme";
   description = "my custom prelude";
